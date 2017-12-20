dc1e6f435ff9f507806af733828cb4455687d1c0
Sugizaki Yukimasa <i.can.speak.c.and.basic@gmail.com>
2017-12-20 15:57:28 +0900

    idein/vc4mem: Describe cache operations
    Signed-off-by: Sugizaki Yukimasa <i.can.speak.c.and.basic@gmail.com>

--- include/uapi/linux/vc4mem.h
+++ include/uapi/linux/vc4mem.h
@@ -27,10 +27,35 @@ struct vc4mem_free_mem {
 	};
 };
 
+/*
+ *   CPU-side cache manipulation
+ *
+ *   There are two operations needed to do when you use the same memory region
+ * for CPU and GPU.
+ *   One is invalidation.  This operation ensures that updates made by GPU are
+ * visible to CPU by marking CPU cache contents as invalid.  So do this when GPU
+ * has just written data to a memory region and CPU is going to read from it.
+ *   The other is clean.  This operation ensures that updates made by CPU are
+ * visible to GPU by writing CPU cache contents to memory.  So do this when CPU
+ * issued store instructions and GPU is going to read from the region to which
+ * CPU wrote.
+ *   For example:
+ *
+ *     uint32_t *p_user, p_dma;
+ *
+ *     p_user = vc4mem_alloc_mem(SIZE, &p_dma);
+ *     if (p_user == NULL) {
+ *         error_and_exit();
+ *     }
+ *     initalize_on_cpu(p_user);
+ *     vc4mem_cpu_cache_op_clean(p_dma, SIZE);
+ *     process_on_gpu(p_dma, SIZE);
+ *     vc4mem_cpu_cache_op_invalidate(p_dma, SIZE);
+ *     read_on_cpu(p_user);
+ */
 typedef unsigned vc4mem_cpu_cache_op_t;
 #define VC4MEM_CPU_CACHE_OP_INVALIDATE   0
 #define VC4MEM_CPU_CACHE_OP_CLEAN        1
-#define VC4MEM_CPU_CACHE_OP_FLUSH        VC4MEM_CPU_CACHE_OP_CLEAN
 
 struct vc4mem_cpu_cache_op {
 	union {
