93ba2935d5ce960a34c5b2b61faa05f315a3cae3
ishiy <y.ishihara.1993@gmail.com>
2025-02-06 14:02:29 +0900

    More debug info

--- drivers/gpu/drm/vc4/vc4_v3d.c
+++ drivers/gpu/drm/vc4/vc4_v3d.c
@@ -501,7 +501,14 @@ static int vc4_v3d_bind(struct device *dev, struct device *master, void *data)
 	pm_runtime_use_autosuspend(dev);
 	pm_runtime_set_autosuspend_delay(dev, 40); /* a little over 2 frames. */
 
+	DRM_INFO("dev->init_name = %s\n", dev->init_name);
+	DRM_INFO("dev->device_type->name = %s\n", dev->device_type->name);
+	DRM_INFO("dev->device_driver->name = %s\n", dev->device_driver->name);
+	DRM_INFO("dev->of_node->name = %s\n", dev->of_node->name);
+	int count = of_count_phandle_with_args(dev->of_node, "firmware", NULL);
+	DRM_INFO("firmware phandle count: %d\n", count);
 	firmware_node = of_parse_phandle(dev->of_node, "firmware", 0);
+	DRM_INFO("firmware_node->name = %s\n", firmware_node->name);
 	vc4->firmware = rpi_firmware_get(firmware_node);
 	of_node_put(firmware_node);
 	if (!vc4->firmware) {
